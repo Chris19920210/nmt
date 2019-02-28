5eccece07d69cddafa6fac783fdb6fdc502e6bf2
Chris <chen3809@umn.edu>
2019-02-28 17:33:46 +0800

    serving utils modification

--- alignment/serving_utils.py
+++ alignment/serving_utils.py
@@ -26,6 +26,8 @@ import tensorflow as tf
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2_grpc
 import abc
+import numpy as np
+
 
 def _make_example(ids,
                   feature_name="sources"):
@@ -113,16 +115,20 @@ def predict(src_list, tgt_list, request_fn, src_encoder, tgt_encoder):
     return predictions
 
 
-def get_src_slice(src_align_ids, src_ids, align_matrix):
-    try:
-        start = src_ids.index(src_align_ids[0])
-        end = src_ids.index(src_align_ids[-1]) + 1
-    except Exception as e:
-        print(str(e))
-        start = 0
-        end = 0
+def indices(mylist, value):
+    return [i for i, x in enumerate(mylist) if x == value]
 
-    return align_matrix[start: end, :]
+
+def get_src_slice(src_align_ids, src_ids, align_matrix):
+    start_list = indices(src_ids, src_align_ids[0])
+    end_list = indices(src_ids, src_align_ids[-1])
+    if len(start_list) != 0 and len(end_list) != 0:
+        ret = np.array(map(lambda args: align_matrix[args[0]: args[1] + 1, :],
+                           zip(start_list, end_list)))
+    else:
+        ret = np.array([])
+
+    return ret
 
 
 class WordSubstitution:
@@ -131,8 +137,8 @@ class WordSubstitution:
         self.tgt_encoder = tgt_encoder
 
     def get_word_src_slice(self, src_word, src_ids, align_matrix):
-        tgt_align_ids = self.src_encoder.encode(src_word)
-        return get_src_slice(tgt_align_ids, src_ids, align_matrix)
+        src_align_ids = self.src_encoder.encode(src_word)
+        return get_src_slice(src_align_ids, src_ids, align_matrix)
 
     @abc.abstractmethod
     def word_alignment(self, word_src_slice):
@@ -142,16 +148,23 @@ class WordSubstitution:
         """
         return 0, 1
 
+    def _substitute_per(self, tgt_ids, word_src_slice):
+        start, end = self.word_alignment(word_src_slice)
+        tgt_slice_id = tgt_ids[start, end]
+        tgt_word = self.tgt_encoder.decode(tgt_slice_id)
+        return tgt_word
+
     def _substitute(self, src_word, tgt_sub_word, src_ids, tgt_ids, align_matrix):
-        word_src_slice = self.get_word_src_slice(src_word, src_ids, align_matrix)
-        if word_src_slice.size == 0:
+        word_src_slices = self.get_word_src_slice(src_word, src_ids, align_matrix)
+        if word_src_slices.size == 0:
             return self.tgt_encoder.decode(tgt_ids)
         else:
-            start, end = self.word_alignment(word_src_slice)
-            tgt_slice_ids = tgt_ids[start: end]
-            tgt_word = self.tgt_encoder.decode(tgt_slice_ids)
+            tgt_words = map(lambda word_src_slice:
+                            self._substitute_per(tgt_ids, word_src_slice), word_src_slices)
             tgt_sentence = self.tgt_encoder.decode(tgt_ids)
-            return tgt_sentence.replace(tgt_word, tgt_sub_word)
+            for tgt_word in tgt_words:
+                tgt_sentence = tgt_sentence.replace(tgt_word, tgt_sub_word)
+            return tgt_sentence
 
     def substitute(self, src_words, tgt_sub_words, src_ids_list, tgt_ids_list, align_matrices):
         return list(map(lambda args: self._substitute(
